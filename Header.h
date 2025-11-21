1c7f74069d905694df5dd4f9b75db9976a1a703c
Tel Mithryn <145218442+nikisubbo@users.noreply.github.com>
2025-11-21 23:27:55 +0500

    Add files via upload
    Заголовочный файл

---
+++ Header.h
@@ -0,0 +1,7 @@
+#pragma once
+using namespace std;
+void printField(int field[10][10]);
+void initilization(int field[10][10]);
+void move(int field[10][10], int player);
+bool win(int field[10][10], int player);
+
